6b93158ce38655191b56acce0c2d67f7317e9eb2
Steve Baker <_steve_@outlook.com>
2017-06-19 10:51:56 +0200

    Allow normal @example tags for php
    Also refactored javadoc code a bit

--- core/Parsing.Javadoc.fs
+++ core/Parsing.Javadoc.fs
@@ -1,21 +1,34 @@
-﻿module private Parsing.Javadoc
+module private Parsing.Javadoc
 
 open Nonempty
 open Parsing.Core
+open Markdown
 open System.Text.RegularExpressions
 
 
+let private tagRegex =
+    Regex(@"^\s*@(\w+)(.*)$")
+
+
 let javadoc settings =
-    splitIntoChunks (beforeRegex (Regex("^\\s*@")))
-        >> Nonempty.collect
-            (fun (Nonempty(firstLine, _) as lines) ->
-                if Line.contains (Regex("^\\s*@example")) firstLine then
-                    Block.ignore lines |> Nonempty.singleton
-                else
-                    // Split after lines that contain only a tag
-                    lines
-                        |> (splitIntoChunks
-                                (afterRegex (Regex("^\\s*@\\w+\\s*$")))
-                            )
-                        |> Nonempty.collect (Markdown.markdown settings)
-            )
+
+    let isStandaloneTag (m: Match) =
+        m.Success && System.String.IsNullOrEmpty(m.Groups.Item(2).Value)
+
+    let splitTaggedSection (Nonempty(headLine, tailLines) as lines) =
+        let m = tagRegex.Match(headLine)
+        if isStandaloneTag m then
+            if m.Groups.Item(1).Value.ToLower() = "example" then
+                Nonempty.singleton (Block.ignore lines)
+            else
+                let tagBlock = Block.Ignore 1
+                match Nonempty.fromList tailLines with
+                    | Some restLines ->
+                        Nonempty.cons tagBlock (markdown settings restLines)
+                    | None ->
+                        Nonempty.singleton tagBlock
+        else
+            markdown settings lines
+
+    splitIntoChunks (beforeRegex tagRegex)
+        >> Nonempty.collect splitTaggedSection
