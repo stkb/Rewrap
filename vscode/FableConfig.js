6a0c1658a233b5e669bf5cb320341e07e8997a18
Steve Baker <_steve_@outlook.com>
2018-02-04 07:27:09 +0100

    Add dev watch task
    Runs fable-splitter in watch mode and runs tests after every
    compilation.

--- vscode/FableConfig.js
+++ vscode/FableConfig.js
@@ -5,6 +5,31 @@ function resolve(relativePath) {
     return path.join(__dirname, relativePath);
 }
  
+function resolve(filePath) {
+  return path.resolve(__dirname, filePath)
+}
+
+function runTests() {
+  try {
+      var scriptPath = resolve("../tests.js");
+      console.log("Running tests")
+      var childProcess = require("child_process");
+      var path = require("path");
+      var cp = childProcess.fork(scriptPath);
+      cp.on("exit", function (code, signal) {
+          if (code === 0) {
+              console.log("Success");
+          } else {
+              console.log("Exit", { code: code, signal: signal });
+          }
+      });
+      cp.on("error", console.error.bind(console));
+  } catch (err) {
+      console.error(err);
+  }
+}
+
+
 module.exports = {
   entry: resolve("Fable.fsproj"),
   outDir: resolve("compiled"),
@@ -12,5 +37,6 @@ module.exports = {
     presets: [[ "env", { targets: { node: "current" }, modules: "commonjs" }]],
     sourceMaps: false,
   }),
-  fable: { }
+  fable: { },
+  postbuild: runTests
 }
