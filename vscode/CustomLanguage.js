b13eed04ec93f15c91c46cd091cd5591dc0596bc
Steve Baker <_steve_@outlook.com>
2020-05-31 15:20:33 +0200

    CustomLanguage: dial back the logging

--- vscode/CustomLanguage.js
+++ vscode/CustomLanguage.js
@@ -4,22 +4,25 @@ const JSON = require('comment-json')
 
 const getConfig = (getText, path) => {
     let config = { line: null, block: null }
+    let warnings = [], error
     try {
-        console.info(`Inspecting JSON file ${path}`)
         const c = JSON.parse(getText(path), null, true).comments
         if(c) {
             // 'line' must be a string or null.
             if(typeof c.lineComment === 'string') config.line = c.lineComment
-            else console.warn("lineComment not a string.")
+            else warnings.push("lineComment not a string.")
             // 'block' must be an array of 2 strings or null.
             if(Array.isArray(c.blockComment) && c.blockComment.length > 1)
                 config.block = c.blockComment.slice(0, 2)
-            else console.warn("blockComment not a length-2 array.")
+            else warnings.push("blockComment not a length-2 array.")
         }
-        else console.warn("No comments block found.")
+        else warnings.push("No comments block found.")
     }
-    catch(err) { 
-        console.error(err)
+    catch(err) { error = err }
+    if(error || warnings.length) {
+        console.info(`Inspecting JSON file ${path}`)
+        if(error) console.error(error.message)
+        warnings.forEach(w => console.warn(w))
     }
     return config
 }
@@ -31,17 +34,10 @@ const createCache = exts => {
         try {
             let obj = ext.packageJSON
             if((obj = obj.contributes) && (obj = obj.languages)) {
-                console.log(`Extension ${ext.id}`)
                 for(const l of obj) {
                     if(!l.id) continue
-                    console.info(`  contributes '${l.id}'`)
-                    if(!l.configuration)
-                        console.info("    but provides no configuration file")
-                    else {
-                        if(cache[l.id]) console.info(`    overwriting ${cache[l.id]}`)
-
+                    if(l.configuration) {
                         const confPath = Path.join(ext.extensionPath, l.configuration)
-                        console.info(`    with ${confPath}`)
                         cache[l.id] = confPath
                     }
                 }
@@ -66,24 +62,14 @@ const getCommentMarkers = (exts, getFileText) => {
     let cache = null
     return lang => {
         cache = cache || createCache(exts)
-        console.info(`Looking for extension for language: ${lang}`)
 
         if (cache[lang]) {
             if(typeof cache[lang] === 'string')
                 cache[lang] = getConfig(getFileText, cache[lang])
-            if(cache[lang].line || cache[lang].block) {
-                console.info("Using.")
-                return cache[lang]
-            }
-            else {
-                console.info("No line or block comment definition found.")
-                return null
-            }
+            if(cache[lang].line || cache[lang].block) return cache[lang]
+            else return null
         }
-        else {
-            console.info("None found.")
-            return null
-        }
+        else return null
     }
 }
 
