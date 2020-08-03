f60c023cf15bf9b461863fe807412ccaa4c17b06
Steve Baker <_steve_@outlook.com>
2020-08-03 12:54:34 +0200

    Add some VSCode tests
    Adds some simple tests to check the settings-retrieval in VSCode. Not
    using a test framework yet. But using the (new) vscode-test module which
    allows running the tests from the command line/CI.

---
+++ vscode.tests/run.js
@@ -0,0 +1,19 @@
+const {resolve} = require('path')
+const {runTests} = require('vscode-test')
+
+async function main() {
+    try {
+        const extensionDevelopmentPath = resolve(__dirname, '..')
+        const extensionTestsPath = resolve(__dirname, 'tests')
+        const workspace = resolve(__dirname, 'fixture')
+        const launchArgs = [workspace, '--disable-extensions']
+
+        // Download VS Code, unzip it and run the integration test
+        await runTests({extensionDevelopmentPath, extensionTestsPath, launchArgs})
+    } catch (err) {
+        console.error('Failed to run tests');
+        process.exit(1);
+    }
+}
+
+main();
---
+++ vscode.tests/tests.js
@@ -0,0 +1,32 @@
+const assert = require('assert')
+const {window, workspace} = require('vscode')
+const getSettings = require('../vscode/Settings')
+
+const testSettings = async (language, expected) => {
+    const doc = await workspace.openTextDocument({language, content: ""})
+    const editor = await window.showTextDocument(doc)
+    const actual = getSettings(editor)
+    assert.deepStrictEqual(actual, expected)
+}
+
+const settingsData = {
+    plaintext: {
+        columns: [45, 90],
+        doubleSentenceSpacing: true,
+        wholeComment: false,
+        reformat: true,
+        tabWidth: 2,
+    },
+    markdown: {
+        columns: [60],
+        doubleSentenceSpacing: false,
+        wholeComment: true,
+        reformat: false,
+        tabWidth: 6,
+    },
+}
+
+exports.run = async () => {
+    // breaks when run in parallel (Promise.all)
+    for([l, e] of Object.entries(settingsData)) { await testSettings(l,e) }
+}
