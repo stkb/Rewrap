e921949d0e7ee6822021e5d36f59c9682b4d56fe
Steve Baker <scsavtnk@gmail.com>
2021-11-09 18:31:49 +0100

    VSC tests: fix path

--- vscode.test/tests.js
+++ vscode.test/tests.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const {window, workspace} = require('vscode')
-const {getCoreSettings} = require('../vscode/bin/Extension.js')
+const {getCoreSettings} = require('../vscode/dist/Extension.js')
 
 const testSettings = async (language, expected) => {
     const doc = await workspace.openTextDocument({language, content: ""})
