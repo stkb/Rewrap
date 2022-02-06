11d4ebc11c8c82d3b00bec04a4d9eb3aaa2cfb34
Steve Baker <scsavtnk@gmail.com>
2022-02-06 19:05:52 +0100

    Add .rmd files as markdown (#245)

--- core/Parsing.Documents.fs
+++ core/Parsing.Documents.fs
@@ -164,7 +164,7 @@ let mutable languages = [
         java
     lang "Lua" "" ".lua" <| sc [block (@"--\[(=*)\[", @"\]$1\]"); line "--"]
     lang "Makefile" "make" "makefile" <| configFile
-    lang "Markdown" "mdx" ".md|.mdx" <| docOf markdown
+    lang "Markdown" "mdx" ".md|.mdx|.rmd" <| docOf markdown
     // MATLAB uses .m but that's already taken for Objective-C
     lang "MATLAB" "" "" <| sc [line "%(?![%{}])"; block ("%\{", "%\}")]
     lang "Objective-C" "" ".m|.mm"
